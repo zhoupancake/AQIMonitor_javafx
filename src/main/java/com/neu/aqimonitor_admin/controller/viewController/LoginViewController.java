5df91bf5e76236151ea1f22868f4bbecb85cc5f5
cody <codychow@foxfile.com>
2025-06-17 19:26:04 +0800

    完成创建User和Administrator的Model类 完成了入口的初步搭建

---
+++ src/main/java/com/neu/aqimonitor_admin/controller/viewController/LoginViewController.java
@@ -0,0 +1,29 @@
+package com.neu.aqimonitor_admin.controller.viewController;
+
+import com.neu.aqimonitor_admin.MainApp;
+import com.neu.aqimonitor_admin.controller.logicController.UserController;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class LoginViewController {
+    @FXML
+    private TextField accountField;
+    @FXML
+    private PasswordField passwordField;
+
+    private MainApp mainApp;
+    private Stage stage;
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    @FXML
+    public void handleLogin() {
+
+    }
+
+}
